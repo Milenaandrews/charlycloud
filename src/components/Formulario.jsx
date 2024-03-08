ca6cb1d803c9eff325f126c3201a60ac23ce32df
Milenaandrews <mileandrews@gmail.com>
2024-03-08 00:49:17 -0300

    avance formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,27 +1,54 @@
 'use client'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {Input, Button } from "@nextui-org/react"
 import ContactoContext from '@/context/Contacto/ContactoContext'
 
 
 
 const Formulario = () => {
+
   const {guardarContacto}= useContext(ContactoContext)
+
+  const initialValues = {
+      nombre: "",
+      farmacia:"",
+      email:""
+  }
+
+  const[user, setUser] = useState(initialValues)
+
+  const handleContact = async (e) =>{
+    e.preventDefault();
+    try { await guardarContacto(user);
+      alert("registro existoso");
+      setUser(initialValues)
+      
+    } catch (error) {
+      console.error("Error durante el registro de usuario", error);
+      alert("registro fallido")
+      
+    }
+  }
+
+  const changeMode = () =>{
+    setUser(initialValues)
+  }
 
   useEffect(() => {
     guardarContacto();
   },[])
     
+
   return (
     <div className='w-full h-screen flex items-center justify-center'>
       <div className='flex bg-slate-700/70 rounded-xl w-auto h-auto flex-col items-center justify-center gap-4 p-3'>
           <h1>Formulario </h1>
-          <Input className='w-80' type="text" label="Nombre de Contacto" required />
+          <Input className='w-80' type="text" label="Nombre de Contacto" required onChange={handleContact} value ={user.nombre}/>
           <Input className='w-80' type="text" label="Farmacia" />
           <Input className='w-80' type="email" label="Email" />
           {/* <Input className='w-80' type="text" label="Ciudad" />
           <Input className='w-80' type="text" label="País" /> */}
-          <Button color="primary" >Enviar</Button>
+          <Button color="primary" onClick={changeMode}>Enviar</Button>
 
       </div>
     
