0eedb369e981161631220979b4e8154945aff100
Milena Andrews <mileandrews@gmail.com>
2024-11-01 22:41:32 -0300

    deploy BFF

--- src/config/axiosClient.jsx
+++ src/config/axiosClient.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL:
-    "https://charly-backend-ndfc2n97a-milenas-projects-5e29101f.vercel.app",
+  baseURL: "https://charly-backend.onrender.com",
 });
 
 export default axiosClient;
